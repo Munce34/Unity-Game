65e2457685cc10e5ce90477ac2d005a1c9c7432f
Munce <hahahabookteam2@gmail.com>
2014-06-29 13:14:33 -0700

    Added sphere player, made moving controls for it

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float playerSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		// Player Spawn Point
+		transform.position = new Vector3 (0, 3, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Player Moving
+		transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime);
+
+		transform.Translate(Vector3.up * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime);
+	
+	}
+}
