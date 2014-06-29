805a7f503b48821c90da2dec139fb46128c9e534
Munce <hahahabookteam2@gmail.com>
2014-06-29 13:51:47 -0700

    Added Floor, wrote code for it

---
+++ Assets/Floor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// Floor to stay in one place
+		transform.position = new Vector3 (0, -3, 0);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
